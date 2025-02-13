fece26f3ccf4e9bda832ad3d0e380fe07368ef7f
Lucas Trindade <trindadebra@gmail.com>
2025-02-13 00:07:26 -0300

    remove login from menu

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -85,14 +85,6 @@ export default function Hero() {
                     </a>
                   ))}
                 </div>
-                <div className="py-6">
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                  >
-                    Log in
-                  </a>
-                </div>
               </div>
             </div>
           </DialogPanel>
