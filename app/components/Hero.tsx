70f7705c3b82ab60cc56a20f1b5ac4e656f03618
Lucas Trindade <trindadebra@gmail.com>
2025-02-13 00:12:41 -0300

    Hero fix

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <div className="bg-white">
+    <div className="bg-white min-h-screen relative">
       <header className="absolute inset-x-0 top-0 z-50">
         <div className="mx-auto max-w-7xl">
           <div className="px-6 pt-6 lg:max-w-2xl lg:pr-0 lg:pl-8">
@@ -91,9 +91,9 @@ export default function Hero() {
         </Dialog>
       </header>
 
-      <div className="relative">
-        <div className="mx-auto max-w-7xl">
-          <div className="relative z-10 pt-14 lg:w-full lg:max-w-2xl">
+      <div className="relative min-h-screen">
+        <div className="mx-auto max-w-7xl h-full">
+          <div className="relative z-10 h-full lg:w-full lg:max-w-2xl">
             <svg
               viewBox="0 0 100 100"
               preserveAspectRatio="none"
@@ -103,18 +103,23 @@ export default function Hero() {
               <polygon points="0,0 90,0 50,100 0,100" />
             </svg>
 
-            <div className="relative px-6 py-32 sm:py-40 lg:px-8 lg:py-56 lg:pr-0">
+            <div className="relative px-6 min-h-screen flex items-center sm:py-40 lg:px-8 lg:py-0 lg:pr-0">
               <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
                 <div className="hidden sm:mb-10 sm:flex">
                   <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                    Conheça nossos serviços e soluções logísticas.{' '}
-                    <a href="#" className="font-semibold whitespace-nowrap text-red-600">
+                    Orçe seu frete agora mesmo sem compromisso.{' '}
+                    <a 
+                      href="https://web.whatsapp.com/send?phone=5511985148306&text=Olá, gostaria de mais informações sobre seus serviços" 
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="font-semibold whitespace-nowrap text-red-600"
+                    >
                       <span aria-hidden="true" className="absolute inset-0" />
-                      Saiba mais <span aria-hidden="true">&rarr;</span>
+                      Solicitar orçamento <span aria-hidden="true">&rarr;</span>
                     </a>
                   </div>
                 </div>
-                <h1 className="text-5xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-7xl">
+                <h1 className="text-5xl font-semibold tracking-tight text-pretty text-gray-900">
                   WTrindade Transportes
                 </h1>
                 <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
@@ -122,14 +127,16 @@ export default function Hero() {
                 </p>
                 <div className="mt-10 flex items-center gap-x-6">
                   <a
-                    href="#"
+                    href="https://web.whatsapp.com/send?phone=5511985148306&text=Olá, gostaria de mais informações sobre seus serviços"
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                   >
-                    Conheça nossos serviços
+                    Entrar em contato!
                   </a>
-                  <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                  {/* <a href="#" className="text-sm/6 font-semibold text-gray-900">
                     Saiba mais <span aria-hidden="true">→</span>
-                  </a>
+                  </a> */}
                 </div>
               </div>
             </div>
@@ -141,7 +148,8 @@ export default function Hero() {
             height={1080}
             alt=""
             src="/assets/images/hero.webp"
-            className="aspect-3/2 object-cover lg:aspect-auto lg:size-full"
+            className="aspect-[3/2] object-cover lg:aspect-auto lg:h-full lg:w-full"
+            priority
           />
         </div>
       </div>
